1ea5a2fba2f11183ecb0aeaf999a77ac4e13ddb0
Tomás Ferreira <tomasrferreira2@gmail.com>
2022-12-29 19:59:38 -0500

    feat(script1): Multply two strings numbers

---
+++ scripts/script1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+
+int main(void)
+{
+    // Declare variables for the two integers and the result
+    mpz_t x, y, result;
+
+    // Declare a string for input
+    char input[100];
+
+    // Initialize the variables
+    mpz_init(x);
+    mpz_init(y);
+    mpz_init(result);
+
+    printf("#### Multiplication of two inserted numbers as string ####\n");
+
+    // Get the first integer from the user
+    printf("Enter the first integer: ");
+    scanf("%s", input);
+    mpz_set_str(x, input, 10);
+
+    // Get the second integer from the user
+    printf("Enter the second integer: ");
+    scanf("%s", input);
+    mpz_set_str(y, input, 10);
+
+    // Calculate the multiplication of the two integers and store the result in "result"
+    mpz_mul(result, x, y);
+
+    // Convert the result to a string and print it
+    char *result_str = mpz_get_str(NULL, 10, result);
+    printf("The result is: %s\n", result_str);
+
+    // Free the result string
+    free(result_str);
+
+    return 0;
+}
