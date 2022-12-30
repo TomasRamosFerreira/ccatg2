07ad5f567f44c25e890ad53dea4b2ec106b4037d
Tomás Ferreira <tomasrferreira2@gmail.com>
2022-12-30 15:22:20 -0500

    feat(script2): Comparation of pow operation using GMP and Iterable function

---
+++ scripts/script6.c
@@ -0,0 +1,91 @@
+#include <gmp.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// Declares the variables needed to store large numbers
+mpz_t result, base;
+
+/**
+ * Author           => Tomás Ferreira
+ * Created At       => 30/12/2022
+ * Last Edited At   => 30/12/2022
+ * Version          => @v1.0 
+ * 
+ * Script that compares the time of a pow operation,
+ * between GMP and Iterable function.
+*/
+
+/**
+ * Function that uses the GMP library 
+ * to calculate the power of a large number raised to an integer power
+ * */ 
+void gmpSolution(int exponent) {
+    mpz_pow_ui(result, base, exponent);
+}
+
+/**
+ * Function that implements an iterative solution, 
+ * to calculate the power of an integer raised to an integer power
+ * */ 
+int iterativeSolution(int base, int exponent) {
+    int result = 1;
+    for (int i = 0; i < exponent; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+/**
+ * Calculate the duration in seconds of the execution
+*/
+double executionTimeResult(clock_t startTime, clock_t endTime) {
+    return (double)(endTime - startTime) / CLOCKS_PER_SEC;
+} 
+
+int main(int argc, char *argv[]) {
+    // Initializes the declared variables
+    int baseInt = 2;
+    mpz_init(result);
+    mpz_init(base);
+
+    // Sets the value of the base to 2
+    mpz_set_ui(base, baseInt);
+
+    int exponent;
+    char continueExecution;
+    
+    // Initiate start and end timers
+    clock_t startTime, endTime;
+
+    // Loop until the user indicates that they want to exit
+    do {
+        // Reads the exponent from the command line
+        printf("Insira o expoente: ");
+        scanf("%d", &exponent);
+        
+        // Measures the execution time of the GMP solution
+        startTime = clock();
+        gmpSolution(exponent);
+        endTime = clock();
+        printf("Execution time of the GMP solution: %f seconds\n", executionTimeResult(startTime, endTime));
+        gmp_printf("GMP solution result: %Zd\n\n", result);
+
+        // Measures the execution time of the iterative solution
+        startTime = clock();
+        int iterativeSolutionResult = iterativeSolution(baseInt, exponent);
+        endTime = clock();
+        printf("Execution time of the iterative solution: %f seconds\n", executionTimeResult(startTime, endTime));
+        printf("Iterative solution result: %d\n", iterativeSolutionResult);
+
+        printf("\n\n");
+
+        // Asks the user if they want to continue execution
+        printf("Deseja continuar a execução? (s/n) ");
+        scanf(" %c", &continueExecution);
+
+        printf("\n\n");
+    } while (continueExecution != 'n');
+
+    return 0;
+}
