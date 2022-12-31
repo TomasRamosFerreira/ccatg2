4adb180e53b52e553a59a807852a6047aeec38aa
Tomás Ferreira <tomasrferreira2@gmail.com>
2022-12-30 21:37:46 -0500

    feat(script8): gets all prime numbers up to a limit given by the user, from the param

---
+++ scripts/script8.c
@@ -0,0 +1,53 @@
+#include <gmp.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "calculatePrimes.c"
+
+/**
+ * Author           => Tomás Ferreira
+ * Created At       => 31/12/2022
+ * Last Edited At   => 31/12/2022
+ * Version          => @v1.0
+ *
+ * Script that gets all prime numbers up to a limit given by the user, from the param.
+ * It's using the Sieve of Eratosthenes algorithm.
+ * This code was inspired by the folowing site:  https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+ */
+
+int main(int argc, char *argv[])
+{
+    // Verifies the number of arguments
+    if (argc != 2)
+    {
+        // Give a user a info how to use the program
+        fprintf(stderr, "Usage: %s <maximum limit>\n", argv[0]);
+        return 1; // end program as error
+    }
+
+    // Declares the variable to store the maximum limit
+    mpz_t maxLimit;
+    unsigned long size;
+
+    // Initialize variable
+    mpz_init(maxLimit);
+
+    // Parses the maximum limit from the string
+    mpz_set_str(maxLimit, argv[1], 10);
+
+    // Calculates the prime numbers
+    mpz_t *primes = calculatePrimes(maxLimit, &size);
+
+    // Prints the prime numbers
+    gmp_printf("Prime numbers up to %Zd:\n", maxLimit);
+    for (unsigned long i = 0; i < size; i++)
+        gmp_printf("%Zd\n", primes[i]);
+
+    // Frees the memory allocated for the large number and the prime numbers list
+    mpz_clear(maxLimit);
+    for (unsigned long i = 0; i < size; i++)
+        mpz_clear(primes[i]);
+    free(primes);
+
+    return 0; // End program successefully
+}
